42de8ec3d7bfaa4c5dd53cf272557df8c89c0856
Aleksa Milenovic <73794793+mileni98@users.noreply.github.com>
2021-08-23 22:09:06 +0200

    Create Appointment.cs

---
+++ Hospital/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int PharmacyId { get; set; }
+        public int PatientId { get; set; }
+        public int MedicalWorkerId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int Rating { get; set; }
+    }
+}
