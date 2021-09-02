6d1b2cd963902797c829e004334b27b204ce1a9d
Aleksa Milenovic <73794793+mileni98@users.noreply.github.com>
2021-09-03 01:32:44 +0200

    Add charts

---
+++ Hospital/Controllers/ChartController.cs
@@ -0,0 +1,53 @@
+﻿using Hospital.Data;
+using Hospital.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NHospital.Controllers
+{
+    public class ChartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ChartController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            var reservations = _context.Reservation.ToList();
+            var dataList = new List<ChartDTO>();
+
+            var thisM = _context.Reservation
+                            .Where(x => x.StartTime > DateTime.Now.AddMonths(-1)
+                                        && x.StartTime < DateTime.Now.AddMonths(1))
+                            .ToList();
+
+            for (var i = 0; i < 8; i++)
+            {
+                ChartDTO chartDTO = new ChartDTO();
+
+                chartDTO.name = DateTime.Now.AddMonths(i - 8).ToString("MMMM");
+                chartDTO.score = _context.Reservation
+                                        .Where(x => x.StartTime >= DateTime.Now.AddMonths(i - 8)
+                                        && x.StartTime <= DateTime.Now.AddMonths(i - 8 + 1)).ToList().Count;
+                dataList.Add(chartDTO);
+            }
+
+            var month = DateTime.Now.ToString("MMMM");
+            ViewData["data"] = JsonSerializer.Serialize(dataList);
+            ViewData["max"] = dataList.Select(x => x.score).Max();
+            return View();
+        }
+    }
+}
---
+++ Hospital/Models/DTO/ChartDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Models.DTO
+{
+    public class ChartDTO
+    {
+        public string name { get; set; }
+        public int score { get; set; }
+    }
+}
